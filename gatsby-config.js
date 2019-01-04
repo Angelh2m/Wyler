bbbb9db55dcf6fc4d6e44605b3059b661c3701b4
Angel Hernandez <ahm@Angels-MBP.lan1>
2019-01-04 16:24:04 -0600

    Add plugin to remove slashes

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,7 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-sass`,
+    `gatsby-plugin-remove-trailing-slashes`,
     `gatsby-plugin-sitemap`,
     {
       resolve: `gatsby-source-filesystem`,
